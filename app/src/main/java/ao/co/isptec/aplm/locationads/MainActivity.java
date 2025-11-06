50e4224301098e0f15e720b819543353a60d21b1
HINNUXone <denilsonultralink1@gmail.com>
2025-11-06 23:38:39 +0100

    Update before map API

--- app/src/main/java/ao/co/isptec/aplm/locationads/MainActivity.java
+++ app/src/main/java/ao/co/isptec/aplm/locationads/MainActivity.java
@@ -1,6 +1,11 @@
 package ao.co.isptec.aplm.locationads;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,61 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.home_activity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        ImageView btnToPerfil = findViewById(R.id.btnToPerfil);
+        btnToPerfil.setOnClickListener(v -> {
+            Intent i = new Intent(this, PerfilAccount.class);
+            startActivity(i);
         });
+
+        ImageButton btnToList = findViewById(R.id.btnToList);
+        btnToList.setOnClickListener(v -> {
+            Intent i = new Intent(this, ListMenu.class);
+            startActivity(i);
+        });
+
+        ImageButton btnToIdea = findViewById(R.id.btnToIdea);
+        btnToIdea.setOnClickListener(v -> {
+            Intent i = new Intent(this, AboutApp.class);
+            startActivity(i);
+        });
+
+        ImageView btnToAddAds = findViewById(R.id.btnToAddAds);
+        btnToAddAds.setOnClickListener(v -> {
+            Intent i = new Intent(this, AddAds.class);
+            startActivity(i);
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // exemplo: mostrar Luanda
+        LatLng luanda = new LatLng(-8.838333, 13.234444);
+
+        mMap.addMarker(new MarkerOptions().position(luanda).title("Luanda"));
     }
 }
