e2816ebcbea25bdcd51b7b55fa362d94ab5cd12c
HINNUXone <denilsonultralink1@gmail.com>
2025-11-16 21:58:42 +0100

    Register supostamente a consumir pela API

---
+++ app/src/main/java/ao/co/isptec/aplm/locationads/network/interfaces/ApiService.java
@@ -0,0 +1,14 @@
+package ao.co.isptec.aplm.locationads.network.interfaces;
+
+import ao.co.isptec.aplm.locationads.network.models.RegisterRequest;
+import ao.co.isptec.aplm.locationads.network.models.VerifyEmailRequest;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+public interface ApiService {
+    @POST("/auth/register")
+    Call<Void> register(@Body RegisterRequest request);
+
+    @POST("auth/verify-email")
+    Call<Void> verifyEmail(@Body VerifyEmailRequest request);
+}
